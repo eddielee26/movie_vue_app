d57178a59f8a317b52c7baea84c2891303b43dad
eddie <eddielee001@gmail.com>
2019-10-07 21:36:02 -0500

    adds movie vue app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,115 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>New Actor</h1>
+    <div>
+      First Name: <input type="text" v-model="newActorFirstName" /><br>
+      Last Name: <input type="text" v-model="newActorLastName" /><br>
+      Gender: <input type="text" v-model="newActorGender" /><br>
+      Age: <input type="text" v-model="newActorAge" /><br>
+    </div>
+
+    <button v-on:click="createActor()">Create Actor</button>
+
+    <h2>All Actors</h2>
+    {{currentActor}}
+
+    <div v-for="actor in actors">
+      <h2>Name: {{ actor.first_name }} {{ actor.last_name }}</h2>
+      <div>
+        <button v-on:click="showActor(actor)">More Info</button>
+      </div>
+      <div v-if="actor === currentActor">
+        <p>Gender: {{ actor.gender }}</p>
+        <p>Age: {{ actor.age }}</p>
+        <h4>Edit Actor</h4>
+        <div>
+          First Name: <input type="text" v-model="actor.first_name" /><br>
+          Last Name: <input type="text" v-model="actor.last_name" /><br>
+          Gender: <input type="text" v-model="actor.gender" /><br>
+          Age: <input type="text" v-model="actor.age" /><br>
+          <button v-on:click="updateActor(actor)">Update</button><br>
+          <button v-on:click="destroyActor(actor)">Destroy</button><br>
+          {{actor}}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      actors: [],
+      currentActor: {},
+      newActorFirstName: "",
+      newActorLastName: "",
+      newActorGender: "",
+      newActorAge: ""
+    };
+  },
+  created: function() {
+    axios.get("/api/actors").then(response => {
+      this.actors = response.data;
+      console.log(this.actors)
+    });
+  },
+  methods: {
+    createActor: function() {
+      var params = {
+        first_name: this.newActorFirstName,
+        last_name: this.newActorLastName,
+        gender: this.newActorGender,
+        age: this.newActorAge
+      };
+      axios
+      .post("/api/actors", params)
+      .then(response => {
+        console.log("Success", response.data);
+        this.actors.push(response.data);
+        this.newActorFirstName = "";
+        this.newActorLastName = "";
+        this.newActorGender = "";
+        this.newActorAge = "";
+      })
+      .catch(error => {
+        console.log(error.response.data.errors);
+      });
+    },
+    showActor: function(actor) {
+      if (this.currentActor === actor) {
+        this.currentActor = {};
+      } else {
+        this.currentActor = actor;
+      }
+    },
+    updateActor: function(actor) {
+      var params = {
+        first_name: actor.first_name,
+        last_name: actor.last_name,
+        gender: actor.gender,
+        age: actor.age
+      };
+      axios.patch("/api/actors/" + actor.id, params)
+        .then(response => {
+          console.log("Success", response.data);
+        })
+        .catch(error => {
+          console.log(error.response.data.errors);
+        });
+    },
+    destroyActor: function(actor) {
+      axios.delete("/api/actors/" + actor.id)
+        .then(response => {
+          console.log("Success", response.data);
+          var index = this.actors.indexOf(actor);
+          this.actors.splice(index, 1);
+        });
+    }
   }
-}
+};
 </script>
+
+
+<!-- NOTE: _actor.json.jb VIEW and actor.rb MODEL edits made to make this code work  -->
